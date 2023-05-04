28a32b4eb162fad70d04c2089a0fa5e6c35e2082
zMad <62236337+zmad55@users.noreply.github.com>
2023-05-05 02:41:44 +0800

    baguio news scraper

--- topicast-back-end/src/app.py
+++ topicast-back-end/src/app.py
@@ -1,7 +1,50 @@
 from flask import Flask, request, jsonify
 
-app = Flask(__name__)
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import pandas as pd
 
+compiled_data = {
+    "title": [],
+    "date": [],
+    "content": []
+}
+
+
+def scrape_sunstar(date):
+
+    articles = driver.find_elements(By.CLASS_NAME, 'col-md-3')
+    list_articles = []
+    for article in articles:
+        title = article.find_element(By.CLASS_NAME, 'article-title').text
+        link = article.find_element(By.TAG_NAME, 'a')
+        date = article.find_element(By.CLASS_NAME, 'article-date').text
+    
+        newl = link.get_attribute('href')
+    
+        driver.implicitly_wait(10)
+        driver.execute_script("window.open('');")
+        driver.switch_to.window(driver.window_handles[1])
+        driver.get(newl)
+        driver.implicitly_wait(10)
+    
+        content = driver.find_element(By.CLASS_NAME, 'article-body').text
+    
+        per_article = {
+            'title': title,
+            'date': date,
+            'content': content
+        }
+    
+        list_articles.append(per_article)
+        df = pd.DataFrame(list_articles)
+    
+        driver.close()
+        driver.switch_to.window(driver.window_handles[0])
+    return None
+
+
+app = Flask(__name__)
 
 @app.route("/api/hello")
 def handle_form_submission():
